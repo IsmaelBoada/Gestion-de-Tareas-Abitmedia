46261d6fb6aa7978560298f494ed02bb3150013e
Ismael Boada <67159346+IsmaelBoada@users.noreply.github.com>
2025-09-16 15:06:55 -0500

    Commit inicial

---
+++ src/test/java/com/prueba_tecnica/abitmedia/AbitmediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba_tecnica.abitmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbitmediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
